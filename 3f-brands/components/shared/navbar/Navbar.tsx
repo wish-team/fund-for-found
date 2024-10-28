c9c872d5725c3158ca3b47e98d6e9d4533ffd886
sama <sghavamzadeh46@gmail.com>
2024-10-28 17:33:49 +0330

    refactor navbar

--- 3f-brands/components/shared/navbar/Navbar.tsx
+++ 3f-brands/components/shared/navbar/Navbar.tsx
@@ -1,99 +1,98 @@
-'use client';
+// NavigationBar.tsx
+"use client";
+import React from "react";
+import Image from "next/image";
+import logo from "@/app/images/logo.svg";
+import Search from "../Search";
+import AvatarDropdown from "./Avatar"; // Import the new component
+
 import {
   Navbar,
   NavbarBrand,
   NavbarContent,
   NavbarItem,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
   Link,
-  DropdownItem,
-  DropdownTrigger,
-  Dropdown,
-  DropdownMenu,
-  Avatar,
 } from "@nextui-org/react";
-import Image from "next/image";
-import logo from "@/app/images/logo.svg";
-import { LuSearch } from "react-icons/lu";
-import Search from "../Search";
-import { useState } from "react";
-import User from "./User"
 
-type NavigationBarProps = {};
+export default function NavigationBar() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
-const NavigationBar: React.FC<NavigationBarProps> = () => {
-  const [isOpen, setIsOpen] = useState(false); // State to manage the dropdown
-
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
+  const menuItems = [
+    "Profile",
+    "Dashboard",
+    "Activity",
+    "Analytics",
+    "System",
+    "Deployments",
+    "My Settings",
+    "Team Settings",
+    "Help & Feedback",
+    "Log Out",
+  ];
 
   return (
-    <Navbar isBordered>
-      <NavbarContent justify="start">
-        <NavbarBrand className="mr-4">
+    <Navbar onMenuOpenChange={setIsMenuOpen}>
+      <NavbarContent>
           <Image src={logo} alt="Logo" />
-        </NavbarBrand>
-        <div className="hidden md:flex md:justify-center md:gap-3">
-          {["Home", "Explore", "About Us", "Help & Support"].map((item, index) => (
-            <NavbarItem key={index} isActive={item === "Customers"}>
-              <Link
-                className="px-2 text-gray-500"
-                color={item === "Customers" ? "secondary" : "foreground"}
-                href="#"
-              >
-                {item}
-              </Link>
-            </NavbarItem>
-          ))}
-        </div>
-        {/* Hamburger Menu for mobile */}
-        <button className="md:hidden" onClick={toggleMenu}>
-          <LuSearch size={24} />
-        </button>
       </NavbarContent>
 
-      {/* Dropdown for mobile menu */}
-      {isOpen && (
-        <div className="md:hidden bg-white p-4 absolute top-full left-0 right-0 shadow-lg">
-          {["Home", "Explore", "About Us", "Help & Support"].map((item, index) => (
-            <NavbarItem key={index}>
-              <Link
-                className="block px-2 py-1 text-gray-500"
-                href="#"
-                onClick={toggleMenu} // Close menu on item click
-              >
-                {item}
-              </Link>
-            </NavbarItem>
-          ))}
-        </div>
-      )}
-
-      <NavbarContent as="div" className="items-center" justify="end">
-        <Search />
-        <Dropdown placement="bottom-end">
-          <DropdownTrigger>
-            <Avatar
-              isBordered
-              as="button"
-              className="transition-transform"
-              color="primary"
-              name="Jason Hughes"
-              size="sm"
-              src="https://avatars.githubusercontent.com/u/30373425?v=4"
-            />
-          </DropdownTrigger>
-          <DropdownMenu className="bg-white border rounded-xl w-[350px]" aria-label="Profile Actions" variant="flat">
-            <DropdownItem key="profile"><User /></DropdownItem>
-            <DropdownItem key="settings">My Settings</DropdownItem>
-            <DropdownItem key="logout" color="danger">Log Out</DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
+      <NavbarContent className="hidden text-sm text-gray3 lg:pe-20 space-x-3 sm:flex justify-start gap-4" justify="center">
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Home
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive>
+          <Link href="#" aria-current="page">
+            Brand and organization
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            About us
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Help & Support
+          </Link>
+        </NavbarItem>
       </NavbarContent>
+      <NavbarContent justify="end">
+        <div className="hidden md:block">
+          <Search />
+        </div>
+        <NavbarItem >
+          <AvatarDropdown userName="Jason Hughes" userEmail="zoey@example.com" /> 
+        </NavbarItem>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden"
+        />
+      </NavbarContent>
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              color={
+                index === 2
+                  ? "primary"
+                  : index === menuItems.length - 1
+                  ? "danger"
+                  : "foreground"
+              }
+              className="w-full"
+              href="#"
+              size="lg"
+            >
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
     </Navbar>
   );
-};
-
-export default NavigationBar;
-
-  
+}
