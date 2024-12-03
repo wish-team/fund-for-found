3810cddd43cf03873fce0f3d85e5e623ddf2e5b0
sama <sghavamzadeh46@gmail.com>
2024-12-03 11:19:44 +0330

    edit next.config.js

--- 3f-brands/next.config.js
+++ 3f-brands/next.config.js
@@ -1,36 +1,7 @@
-// if(proccess.evn=="developmnet"){
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   fastRefresh: true,
-//   reactStrictMode: true,
-//   // Add this if you're using app directory
-//   experimental: {
-//     reactRefresh: true,
-//     appDir: true,
-//   },
-// };
-
-// module.exports = nextConfig;
-// }
-
-// /**
-//  * @type {import('next').NextConfig}
-//  */
-// const nextConfig = {
-//   /* config options here */
-// }
- 
-// module.exports = nextConfig
-
-
-
-
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   transpilePackages: ["@nextui-org/react"]
 }
 
