0758923b138441d4ac5b96c97573476f7212a789
sama <sghavamzadeh46@gmail.com>
2024-12-20 15:35:57 +0330

    change the focus style of search navbar

--- 3f-brands/app/creators/[id]/page.tsx
+++ 3f-brands/app/creators/[id]/page.tsx
@@ -1,11 +1,11 @@
-'use client';
-import React, { useRef, RefObject } from 'react';
+"use client";
+import React, { useRef, RefObject } from "react";
 import AboutPage from "@/components/pages/creators/about-section/AboutPage";
-import CreatorBanner from "@/components/pages/creators/banner/CreatorBanner";
+import CreatorBanner from "@/components/separate/creators/banner/CreatorBanner";
 import { TierManagement } from "@/components/pages/creators/contributors/TierManagement";
 import FAQ from "@/components/pages/creators/faq-section/FAQsection";
 import TeamMemberInvite from "@/components/pages/creators/team-invite/TeamMemberInvite";
-import Updates from '@/components/pages/creators/updates-section/Updates';
+import Updates from "@/components/pages/creators/updates-section/Updates";
 
 const CreatorProfile = () => {
   const contributionTierRef = useRef<HTMLDivElement>(null);
@@ -17,18 +17,22 @@ const CreatorProfile = () => {
 
   const handleTabClick = (ref: RefObject<HTMLDivElement>) => {
     if (ref.current) {
-      ref.current.scrollIntoView({ behavior: 'smooth' });
+      ref.current.scrollIntoView({ behavior: "smooth" });
     }
   };
 
   return (
-    <div className='flex flex-col gap-20'>
+    <div className="flex flex-col gap-20">
       <CreatorBanner />
       <div className="text-gray3 bg-white z-10 mt-20  flex justify-center md:space-x-12 py-4 border-b overflow-x-scroll space-x-4">
-        <button onClick={() => handleTabClick(contributionTierRef)}>CONTRIBUTION TIER</button>
+        <button onClick={() => handleTabClick(contributionTierRef)}>
+          CONTRIBUTION TIER
+        </button>
         <button onClick={() => handleTabClick(aboutRef)}>ABOUT</button>
         <button onClick={() => handleTabClick(teamRef)}>TEAM</button>
-        <button onClick={() => handleTabClick(topBackersRef)}>TOP BACKERS & CONTRIBUTORS</button>
+        <button onClick={() => handleTabClick(topBackersRef)}>
+          TOP BACKERS & CONTRIBUTORS
+        </button>
         <button onClick={() => handleTabClick(faqRef)}>FAQ</button>
         <button onClick={() => handleTabClick(updatesRef)}>UPDATES</button>
       </div>
@@ -41,9 +45,7 @@ const CreatorProfile = () => {
       <div ref={teamRef}>
         <TeamMemberInvite />
       </div>
-      <div ref={topBackersRef}>
-        {/* Top Backers & Contributors content */}
-      </div>
+      <div ref={topBackersRef}>{/* Top Backers & Contributors content */}</div>
       <div ref={faqRef}>
         <FAQ />
       </div>
@@ -55,4 +57,3 @@ const CreatorProfile = () => {
 };
 
 export default CreatorProfile;
-
--- 3f-brands/components/pages/creators/about-section/components/AboutContentDisplay.tsx
+++ 3f-brands/components/pages/creators/about-section/components/AboutContentDisplay.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button } from "@nextui-org/react";
 import { HiOutlinePencil } from "react-icons/hi";
-import CreatorsTitle from "../../../creators/title/CreatorsTitle";
+import CreatorsTitle from "../../title/CreatorsTitle";
 import { ContentData, EditorBlock } from "../types/content.types";
 import { renderContentBlock } from "../utils/content-renderer";
 import { OutputBlockData } from "@editorjs/editorjs";
