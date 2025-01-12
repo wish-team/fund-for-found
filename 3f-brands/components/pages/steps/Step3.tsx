b29071e9f03fb557a4102a59469bc68741902838
sama <sghavamzadeh46@gmail.com>
2025-01-12 15:30:30 +0330

    add i18n to step3

--- 3f-brands/components/pages/steps/Step3.tsx
+++ 3f-brands/components/pages/steps/Step3.tsx
@@ -1,17 +1,20 @@
 import Title from "../../shared/Title";
 import Image from "next/image";
 import Step3Img from "../../../app/images/steps/brandStep3.svg"
+import { useTranslation } from "react-i18next";
 
 const Step3 = () => {
+  const {t} = useTranslation();
   return (
     <section className="mt-12 flex flex-col items-center">
       <Title 
       title="Waiting to approve"
+      titleKey="step3.title"
       fontWeight="font-light"
       />
       <Image src={Step3Img} className="max-w-[350px] py-8" alt="Waiting to approve" />
-      <h4 className="text-gray3 font-medium text-2xl">You will be notified as soon as it is approved</h4>
-      <p className="text-gray4 font-light text-sm pt-2">Your information is under review and will be confirmed within 2 to 5 business days.</p>
+      <h4 className="text-gray3 font-medium text-2xl">{t("step3.descripton")}</h4>
+      <p className="text-gray4 font-light text-sm pt-2">{t("step3.info")}</p>
     </section>
   )
 }
