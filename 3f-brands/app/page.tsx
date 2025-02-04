69147c890f384140f163ab3ca7ad4e784135662f
sama <sghavamzadeh46@gmail.com>
2025-02-04 11:25:22 +0330

    home page test

--- 3f-brands/app/page.tsx
+++ 3f-brands/app/page.tsx
@@ -19,6 +19,7 @@ function urlBase64ToUint8Array(base64String: string) {
 }
 
 
+
 function PushNotificationManager() {
   const [isSupported, setIsSupported] = useState(false)
   const [subscription, setSubscription] = useState<PushSubscription | null>(
@@ -135,32 +136,32 @@ function InstallPrompt() {
   )
 }
  
-export default function Page() {
-  return (
-    <div>
-      <PushNotificationManager />
-      <InstallPrompt />
-    </div>
-  )
-}
-
-// export default async function Index() {
+// export default function Page() {
 //   return (
-//     <div className="flex-1 w-full flex flex-col gap-20 items-center">
-//       <div className="flex flex-col max-w-5xl px-6 md:p-6">
-//         <main className=" flex items-center flex-col gap-3">
-//           <Title
-//             fontWeight="font-extrabold"
-//             paddingTop="pt-8"
-//             titleKey="home.title"
-//             descKey="home.description"
-//             title="Create your profile and take the first step towards new opportunities"
-//             desc="By creating your account, you'll gain access to a thriving community where brands and individuals are committed to offering you ongoing support. This support network will empower you with the resources, guidance, and connections you need to succeed, ensuring that you’re never alone on your journey."
-//             descSize="text-base"
-//           />
-//           <Card />
-//         </main>
-//       </div>
+//     <div>
+//       <PushNotificationManager />
+//       <InstallPrompt />
 //     </div>
-//   );
+//   )
 // }
+
+export default async function Index() {
+  return (
+    <div className="flex-1 w-full flex flex-col gap-20 items-center">
+      <div className="flex flex-col max-w-5xl px-6 md:p-6">
+        <main className=" flex items-center flex-col gap-3">
+          <Title
+            fontWeight="font-extrabold"
+            paddingTop="pt-8"
+            titleKey="home.title"
+            descKey="home.description"
+            title="Create your profile and take the first step towards new opportunities"
+            desc="By creating your account, you'll gain access to a thriving community where brands and individuals are committed to offering you ongoing support. This support network will empower you with the resources, guidance, and connections you need to succeed, ensuring that you’re never alone on your journey."
+            descSize="text-base"
+          />
+          <Card />
+        </main>
+      </div>
+    </div>
+  );
+}
