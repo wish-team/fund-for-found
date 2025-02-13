217a26adf09d080a8eb118877bdfb887180d8547
sama <sghavamzadeh46@gmail.com>
2025-02-13 18:31:44 +0330

    Temporarily disable push notification functionality

--- 3f-brands/app/page.tsx
+++ 3f-brands/app/page.tsx
@@ -17,7 +17,7 @@ export default async function Index() {
             descSize="text-base"
           />
           <Card />
-          <PushNotificationManager />
+          {/* <PushNotificationManager /> */}
         </main>
       </div>
     </div>
