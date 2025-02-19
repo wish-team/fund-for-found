8eac3165d8008d698827dd81ecf753aac539e789
sama <sghavamzadeh46@gmail.com>
2025-02-19 11:46:17 +0330

    Remove i18next-browser-languagedetector

--- 3f-brands/utils/i18n/utils/i18n.ts
+++ 3f-brands/utils/i18n/utils/i18n.ts
@@ -3,7 +3,6 @@
 
 import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
 import { getOptions } from "../config/settings";
 import { LanguageCode } from "../config/fonts";
 import enTranslations from "../locales/en/translation.json";
@@ -11,30 +10,23 @@ import enTranslations from "../locales/en/translation.json";
 const i18nInstance = i18next.createInstance();
 
 if (!i18nInstance.isInitialized) {
-  i18nInstance
-    .use(LanguageDetector)
-    .use(initReactI18next)
-    .init({
-      ...getOptions(),
-      resources: {
-        en: {
-          translation: enTranslations,
-        },
-      } as Record<LanguageCode, { translation: typeof enTranslations }>,
-      load: "languageOnly",
-      fallbackLng: "en",
-      preload: ["en"],
-      react: {
-        useSuspense: false,
+  i18nInstance.use(initReactI18next).init({
+    ...getOptions(),
+    resources: {
+      en: {
+        translation: enTranslations,
       },
-      interpolation: {
-        escapeValue: false,
-      },
-      detection: {
-        order: ["localStorage", "navigator"],
-        caches: ["localStorage"],
-      },
-    });
+    } as Record<LanguageCode, { translation: typeof enTranslations }>,
+    load: "languageOnly",
+    fallbackLng: "en",
+    preload: ["en"],
+    react: {
+      useSuspense: false,
+    },
+    interpolation: {
+      escapeValue: false,
+    },
+  });
 }
 
 export default i18nInstance;
