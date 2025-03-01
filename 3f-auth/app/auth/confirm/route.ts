9810fd33baa0bab92233cd4d8360390a4847a447
Bardia <Bardiaa830@gmail.com>
2025-03-01 20:11:36 +0330

    Update email code with new SMTP configuration

--- 3f-auth/app/auth/confirm/route.ts
+++ 3f-auth/app/auth/confirm/route.ts
@@ -1,15 +1,15 @@
 import { type EmailOtpType } from '@supabase/supabase-js'
-import type { NextRequest } from 'next/server'
-import { NextResponse } from 'next/server'
+import { type NextRequest } from 'next/server'
+
 import { createClient } from '@/utils/supabase/server'
+import { redirect } from 'next/navigation'
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url)
   const token_hash = searchParams.get('token_hash')
   const type = searchParams.get('type') as EmailOtpType | null
   const next = searchParams.get('next') ?? '/'
-  const redirectTo = request.nextUrl.clone()
-  redirectTo.pathname = next
+
   if (token_hash && type) {
     const supabase = await createClient()
 
@@ -18,10 +18,11 @@ export async function GET(request: NextRequest) {
       token_hash,
     })
     if (!error) {
-      return NextResponse.redirect(redirectTo)
+      // redirect user to specified redirect URL or root of app
+      redirect(next)
     }
   }
 
-  redirectTo.pathname = '/auth/auth-code-error'
-  return NextResponse.redirect(redirectTo)
+  // redirect the user to an error page with some instructions
+  redirect('/auth/auth-code-error')
 }
