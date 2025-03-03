3c5583f85edd253157fdd4b5916c76b2a95f09e0
Bardia <Bardiaa830@gmail.com>
2025-03-03 16:31:33 +0330

    change origin

--- 3f-back/src/main.ts
+++ 3f-back/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:3001',
     credentials: true,
   });
   await app.listen(3010);
