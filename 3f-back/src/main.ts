d69be2f4181401a896013c9c6dea3881c17eaa20
Bardia <Bardiaa830@gmail.com>
2025-03-13 14:10:19 +0330

    Allow all cores to access

--- 3f-back/src/main.ts
+++ 3f-back/src/main.ts
@@ -5,7 +5,13 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  app.enableCors();
+  app.enableCors({
+    origin: '*', // Allow all origins during testing
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
+    exposedHeaders: ['Set-Cookie'],
+  });
   await app.listen(3010);
 }
 bootstrap();
